f4ba23cf45231d84c728c4c4c325880e856ea44a
Daniel <daniel.web19@outlook.com>
2020-09-30 19:21:10 -0500

    first version

---
+++ precache-manifest.dc4874c38196c6ebeb7d278a9513cc97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb7348957ebb26468b6c04e51de84d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34406698e045f82921c5",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "13ddb67d48417d509cb9",
+    "url": "/static/css/main.9784b2cd.chunk.css"
+  },
+  {
+    "revision": "34406698e045f82921c5",
+    "url": "/static/js/2.51678a2a.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/static/js/2.51678a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ddb67d48417d509cb9",
+    "url": "/static/js/main.28c3f0bf.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
